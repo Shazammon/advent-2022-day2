15c8d5c7f53ec8881be6ebafe5502c216ccfa066
Shazammon <ammonorgill@gmail.com>
2022-12-12 13:44:58 -0700

    udpated switch case

--- solution.js
+++ solution.js
@@ -6,4 +6,41 @@ let data = fs.readFileSync('data.txt', 'utf-8')
 
 const stringData = data.split(/\r?\n/)
 
-console.log(stringData)
+// console.log(stringData)
+let score = 0
+
+function calculatePoints() {
+    for (let i = 0; i < stringData.length; i++) {
+        switch (i) {
+            case 'A X':
+                score =  4;
+                break;
+            case 'A Y':
+                score = 8;
+                break;
+            case 'A Z':
+                score = 3;
+                break;
+            case 'B X':
+                score = 1;
+                break;
+            case 'B Y':
+                score = 5;
+                break;
+            case 'B Z':
+                score = 9;
+                break;
+            case 'C X':
+                score = 7;
+                break;
+            case 'C Y':
+                score = 2;
+                break;
+            case 'C Z':
+                score = 6;
+                break;
+        }
+    }
+
+    
+}
